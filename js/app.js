f8af13e83854be2dbd0d0ae3eff76571afea8eae
Abdelrhman moataz <76167138+apdo60311@users.noreply.github.com>
2022-12-17 20:58:38 +0200

    git commit the page

---
+++ js/app.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded' , ()=>{
+
+const nl = document.querySelector("#navbar__list");
+const sections = Array.from(document.querySelectorAll('section'));
+
+for (let i of sections) {
+    let l_item = document.createElement('li');
+    l_item.innerHTML = `<li> <a href="#${i.id}" data-nav="${i.id}" class="menu__link"> ${i.id} </a> </li>`;
+    nl.appendChild(l_item); 
+}
+
+
+// change active section on scrolling
+
+document.onscroll = () => {
+    const sections = Array.from(document.querySelectorAll('section'));
+
+    for (let sec of sections) {
+        if(sec.getBoundingClientRect().top >= -400 && sec.getBoundingClientRect().top <= 150) {
+            sec.classList.add('your-active-class');
+        }
+        else {
+            sec.classList.remove('your-active-class');
+        }
+    }
+};
+document.getElementById('topBtn').style.visibility = 'hidden';
+
+
+});
+
+
+// button to scroll to the top of the page
+function scroltop() {
+    window.scrollTo(0, 0);
+}
+
+window.onscroll = ()=> {
+    if(window.scrollY > 200) {
+        document.getElementById('topBtn').style.visibility = 'visible';
+    }
+    else{
+        document.getElementById('topBtn').style.visibility = 'hidden';
+    }
+}
